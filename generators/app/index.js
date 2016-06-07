7d3386759cdae6c35f5a36ae3678460e9527483f
7sharp9 <kukulcanenator@googlemail.com>
2016-06-07 01:06:35 +0100

    Updated gitignore filename

--- generators/app/index.js
+++ generators/app/index.js
@@ -59,7 +59,7 @@ module.exports = yeoman.Base.extend({
       );
 
     this.fs.copy(
-        this.templatePath('.gitignore'),
+        this.templatePath('gitignore'),
         this.destinationPath('.gitignore')
       );
 
